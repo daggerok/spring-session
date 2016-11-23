7c1f93d0726a2e6e8c5decd9a791ce825f6e44f8
Maksim Kostromin <daggerok@gmail.com>
2016-11-23 19:18:42 +0200

    set user password to pwd

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ sticky-sessions [![build](https://travis-ci.org/daggerok/spring-session.svg?bran
 git clone https://github.com/daggerok/spring-session-demo.git
 docker-compose up -d
 gradle clean build bootRun # if you have gradle
-open http://localhost:8080/session/get
+open http://localhost:8080
+curl -v http://user:pwd@localhost:8080
+curl -vu user:pwd http://localhost:8080
+http -a user:pwd :8080
 ```
--- src/main/java/demo/config/SpringSessionConfig.java
+++ src/main/java/demo/config/SpringSessionConfig.java
@@ -1,8 +1,17 @@
 package demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
 
 @Configuration
 @EnableRedisHttpSession
-public class SpringSessionConfig {}
+public class SpringSessionConfig {
+
+    @Bean
+    public HttpSessionStrategy httpSessionStrategy() {
+        return new CookieHttpSessionStrategy();
+    }
+}
